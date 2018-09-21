a9b0bb903a8ed0046d503d3d28b689937990eb9d
Douglas Lopes <dwillian.lopes@gmail.com>
2018-09-20 23:44:57 -0300

    feat: creating the base structure for the app

---
+++ src/api/FourSquareAPI.js
@@ -0,0 +1,9 @@
+const api = "https://api.foursquare.com/v2/venues/explore?";
+const clientId = "2DOVXTTK4QSNGJZJ203I0XTJZ5U01YZWAJGKAXO2GU00TP2U";
+const clientSecret = "2RXJP4WVNN24ZCSWUFBWUPELCEWL3YGAWBF51JXVMZGGPBPX";
+
+export const getPlace = (query) => 
+    fetch(`${api}client_id=${clientId}&client_secret=${clientSecret}&v=20180323&limit=1&ll=40.7243,-74.0018&query=${query}`)
+    .then( data => data.json())
+    .then( res => res.response)
+    .catch( () => { error: true; })
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import { Navbar, Jumbotron, Button, Grid } from 'react-bootstrap';
-import logo from './logo.svg';
+import logo from './../../logo.svg';
+import * as FourSquareAPI from '../../api/FourSquareAPI';
 import './App.css';
 
 class App extends Component {
+  state = {
+    error: null,
+    isLoaded:false,
+    places: []
+  }
+
+  componentDidMount() {
+    FourSquareAPI.getPlace('coffee').then( places => {
+      if(!('error' in places)){
+        this.setState({
+          isLoaded: true,
+          places: places
+        })
+        console.log(this.state);
+      } else {
+        this.setState({
+          isLoaded: false,
+          places: []
+        })
+      }
+    });
+  }
+
   render() {
     return (
       <div>
